1e14cf1af188c4fd17b503f879cb0b1952cd2df0
VoOneOne <golovano.v.mir@gmail.com>
2024-09-12 12:00:11 +0300

    fux interval

--- src/Paste/Entity/ExpirationTime.php
+++ src/Paste/Entity/ExpirationTime.php
@@ -8,7 +8,7 @@ enum ExpirationTime: int
     case WITHOUT_LIMIT = 0;
     case TEN_MIN = 10;
     case HOUR = 60;
-    case DAY = 86400; // = 24 * 60 * 60;
-    case WEEK = 604800; // = 7 * 24 * 60 * 60
-    case MONTH = 18144000; // = 30 * 7 * 24 * 60 * 60;
+    case DAY = 1440; // = 24 * 60;
+    case WEEK = 10080; // = 7 * 24 * 60
+    case MONTH = 43200; // = 30 * 24 * 60;
 }
